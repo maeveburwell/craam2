abf171337a78d8411cd1bc550bfcea7ec6e53acc
Maeve Burwell <mmb1177@usnh.edu>
2024-11-19 15:00:50 -0500

    got rid of some comments

--- craam/optimization/optimization.jl
+++ craam/optimization/optimization.jl
@@ -134,22 +134,21 @@ function worst_case_l1(z::Vector{Float64}, pbar::Vector{Float64}, xi::Float64)
     @assert sum(pbar) == 1 "Values of pbar must sum to one"
 
     xi = clamp(xi, 0, 2)
-    size = length(z) #size = 4
-    sorted_ind = sortperm(z) #creates a list of the indexes of z sorted by the corresponding values in ascending order
-                             #e.g. z = [0.1, 0.6, 0.2, 0.5] -> sorted_ind = [1, 4, 2, 3]
+    size = length(z)
+    sorted_ind = sortperm(z)
 
-    out = copy(pbar) #duplicate it
-    k = sorted_ind[1] #index begins at 1: e.g. from before, k = 1
+    out = copy(pbar)
+    k = sorted_ind[1]
 
-    epsilon = min(xi / 2, 1 - pbar[k]) #1st iteration: min(0.5/2, 1 - 0.25), though tbf, pbar[k] for all k is going to be 0.25
-    out[k] += epsilon #out[k] for the first iteration would return 0.5 (out[1])
+    epsilon = min(xi / 2, 1 - pbar[k])
+    out[k] += epsilon
     i = size
 
     while epsilon > 0 && i > 0
-        k = sorted_ind[i] #second iteration:  k = sorted_ind[3] = 2 #third iteration: k = sorted[2] = 4
-        difference = min(epsilon, out[k]) #min(0.25, out[2]) = min(0.25,0.25) = 0.25 # third iteration: min(0, 0.25) = 0
-        out[k] -= difference #out[2] - 0.25 = 0.25 - 0.25 = 0 #third iteration: out[4] = out[4] - 0 = 0.25
-        epsilon -= difference #0 -> 0
+        k = sorted_ind[i]
+        difference = min(epsilon, out[k])
+        out[k] -= difference
+        epsilon -= difference
         i -= 1
     end
 
