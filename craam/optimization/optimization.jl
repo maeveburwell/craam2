8ce738d9591516b4fd539e413de7b7dfecf2d920
Maeve Burwell <mmb1177@usnh.edu>
2024-11-25 18:44:03 -0500

    Deleted temp.jl, replaced variable names with symbols, changed name worst_case_l1 to worstcase_l1

--- craam/optimization/optimization.jl
+++ craam/optimization/optimization.jl
@@ -1,6 +1,5 @@
 using LinearAlgebra
 using DataStructures
-using Printf
 using Infinity
 
 struct GradientsL1_w
@@ -12,7 +11,7 @@ struct GradientsL1_w
 end
 
 function GradientsL1_w(z::Vector{Float64}, w::Vector{Float64})
-    epsilon = 1e-8
+    ϵ = 1e-8
     element_count = Int(length(z))
 
     @assert length(w) == element_count
@@ -28,7 +27,7 @@ function GradientsL1_w(z::Vector{Float64}, w::Vector{Float64})
     smallest_w = Inf
 
     for iz in z_increasing
-        @assert w[iz] > epsilon
+        @assert w[iz] > ϵ
         if w[iz] < smallest_w
             push!(possible_receivers, iz)
             smallest_w = w[iz]
@@ -38,27 +37,23 @@ function GradientsL1_w(z::Vector{Float64}, w::Vector{Float64})
     # Computing grads for donor-receiver pairs
     for i = 1:element_count
         for j in possible_receivers
-            if z[i] <= z[j]
-                continue
-            end
-            # Case a: donor ≤ pbar value
+            if z[i] <= z[j] continue end
+            # Case a: donor ≤ p̄ value
             grad = (-z[i] + z[j]) / (w[i] + w[j])
-            push!(grads, grad < -epsilon ? grad : 0)
+            push!(grads, grad < -ϵ ? grad : 0)
             push!(donors, i)
             push!(receivers, j)
             push!(donor_greater, false)
         end
     end
 
-    # Case b: donor > pbar value
+    # Case b: donor > p̄ value
     for i in possible_receivers
         for j in possible_receivers
-            if z[i] <= z[j]
-                continue
-            end
-            if abs(w[i] - w[j]) > epsilon && w[i] < w[j]
+            if z[i] <= z[j] continue end
+            if abs(w[i] - w[j]) > ϵ && w[i] < w[j]
                 grad = (-z[i] + z[j]) / (-w[i] + w[j])
-                push!(grads, grad < -epsilon ? grad : 0)
+                push!(grads, grad < -ϵ ? grad : 0)
                 push!(donors, i)
                 push!(receivers, j)
                 push!(donor_greater, true)
@@ -77,15 +72,14 @@ function steepest_solution(gradients::GradientsL1_w, index::Int)
     return gradients.grads[e], gradients.donors[e], gradients.receivers[e], gradients.donor_greater[e]
 end
 
-function worstcase_l1_w(z::Vector{Float64}, pbar::Vector{Float64}, w::Vector{Float64}, xi::Float64)
-    @assert maximum(pbar) <= 1 + 1e-9 && minimum(pbar) >= -1e-9 "values must be between 0 and 1"
-    @assert xi >= 0 "xi must be nonnegative"
-    @assert length(z) > 0 && length(z) == length(pbar) "z's values needs to be same length as pbar's values"
-    @assert sum(pbar) == 1 "Values of pbar must sum to one"
+function worstcase_l1_w(z::Vector{Float64}, p̄::Vector{Float64}, w::Vector{Float64}, ξ::Float64)
+    @assert maximum(p̄) <= 1 + 1e-9 && minimum(p̄) >= -1e-9 "values must be between 0 and 1"
+    @assert ξ >= 0 "ξ must be nonnegative"
+    @assert length(z) > 0 && length(z) == length(p̄) "z's values needs to be same length as p̄'s values"
+    @assert isapprox(sum(p̄), 1, atol=1e-5) "Values of p̄ must sum to one"
 
-    epsilon = 1e-10
-    p = copy(pbar)
-    xi_rest = xi
+    ϵ = 1e-10
+    xi_rest = ξ
     grad_epsilon = 1e-5
     grad_que = [] #tuple
 
@@ -104,51 +98,50 @@ function worstcase_l1_w(z::Vector{Float64}, pbar::Vector{Float64}, w::Vector{Flo
 
             if receiver == 0 continue end
 
-            if donor_greater && pbar[donor] <= pbar[donor] + epsilon continue end
+            if donor_greater && p̄[donor] <= p̄[donor] + ϵ continue end
 
-            if !donor_greater && pbar[donor] > pbar[donor] + epsilon continue end
+            if !donor_greater && p̄[donor] > p̄[donor] + ϵ continue end
 
-            if pbar[donor] < epsilon continue end
+            if p̄[donor] < ϵ continue end
 
             weight_change = donor_greater ? (-w[donor] + w[receiver]) : (w[donor] + w[receiver])
             @assert weight_change > 0
 
             donor_step = min(xi_rest / weight_change, 
-                             pbar[donor] > pbar[donor] + epsilon ? (pbar[donor] - pbar[donor]) : pbar[donor])
-            pbar[donor] -= donor_step
-            pbar[receiver] += donor_step
+                             p̄[donor] > p̄[donor] + ϵ ? (p̄[donor] - p̄[donor]) : p̄[donor])
+            p̄[donor] -= donor_step
+            p̄[receiver] += donor_step
             xi_rest -= donor_step * weight_change
 
-            if xi_rest < epsilon break end
+            if xi_rest < ϵ break end
         end
-        if xi_rest < epsilon break end
+        if xi_rest < ϵ break end
     end
 
-    objective = dot(pbar, z)
-    return (pbar, objective)
+    return (p̄, dot(p̄, z))
 end
-function worst_case_l1(z::Vector{Float64}, pbar::Vector{Float64}, xi::Float64)
-    @assert maximum(pbar) <= 1 + 1e-9 && minimum(pbar) >= -1e-9 "values must be between 0 and 1"
-    @assert xi >= 0 "xi must be nonnegative"
-    @assert length(z) > 0 && length(z) == length(pbar) "z's values needs to be same length as pbar's values"
-    @assert sum(pbar) == 1 "Values of pbar must sum to one"
+function worstcase_l1(z::Vector{Float64}, p̄::Vector{Float64}, ξ::Float64)
+    @assert maximum(p̄) <= 1 + 1e-9 && minimum(p̄) >= -1e-9 "values must be between 0 and 1"
+    @assert ξ >= 0 "xi must be nonnegative"
+    @assert length(z) > 0 && length(z) == length(p̄) "z's values needs to be same length as p̄'s values"
+    @assert isapprox(sum(p̄), 1, atol=1e-5) "Values of p̄ must sum to one"
 
-    xi = clamp(xi, 0, 2)
+    ξ = clamp(ξ, 0, 2)
     size = length(z)
     sorted_ind = sortperm(z)
 
-    out = copy(pbar)
+    out = copy(p̄)
     k = sorted_ind[1]
 
-    epsilon = min(xi / 2, 1 - pbar[k])
-    out[k] += epsilon
+    ϵ = min(ξ / 2, 1 - p̄[k])
+    out[k] += ϵ
     i = size
 
-    while epsilon > 0 && i > 0
+    while ϵ > 0 && i > 0
         k = sorted_ind[i]
-        difference = min(epsilon, out[k])
+        difference = min(ϵ, out[k])
         out[k] -= difference
-        epsilon -= difference
+        ϵ -= difference
         i -= 1
     end
 
--- craam/optimization/test_code.jl
+++ craam/optimization/test_code.jl
@@ -1,24 +1,29 @@
 include("optimization.jl")  # Include the file containing the worstcase_l1 function
 
+using Distributions
+
 # Test case 1
 z = [0.5, 0.2, 0.9, 0.1]
-pbar = [0.1, 0.25, 0.4, 0.25]
-xi = 0.5
+p̄ = [0.1, 0.25, 0.4, 0.25]
+ξ = 0.5
 
-p_opt, objective_value = worst_case_l1(z, pbar, xi)
+p_opt, objective_value = worst_case_l1(z, p̄, ξ)
 println("For z values: ", z)
-println("For pbar values: ", pbar)
+println("For pbar values: ", p̄)
 println("")
-println("Test case 1 (worst_case_l1)")
+println("Test case 1 (worstcase_l1)")
 println("Optimal p: ", p_opt)
 println("Objective value: ", objective_value)
 println()
-z2 = [0.5, 0.2, 0.9, 0.1]   # Target distribution
-pbar2 = [0.1, 0.25, 0.4, 0.25] # Initial probability distribution
-w = [1.0, 1.0, 1.0, 1.0]    # Weights associated with each probability
-xi2 = 0.5
 
-result = worstcase_l1_w(z2, pbar2, w, xi2)
+# Test case 2
+
+z2 = [0.5, 0.2, 0.9, 0.1]   # Target distribution
+p̄2 = [0.1, 0.25, 0.4, 0.25] # Initial probability distribution
+w = [1.0, 1.0, 1.0, 1.0]    # Weights associated with each probability
+ξ2 = 0.5
+
+result = worstcase_l1_w(z2, p̄2, w, ξ2)
 
 @assert result[1] == p_opt #worst_case_l1 optimal values should be the same as worstcase_l1_w with uniform weights
 @assert result[2] == objective_value #objectives should also be the same
@@ -29,11 +34,28 @@ println("Updated Probability Distribution: ", result[1])
 println("Objective Value: ", result[2])
 println("")
 
+# Test case 3
+
 w2 = [1.0, 2.0, 3.0, 4.0]
 
-result2 = worstcase_l1_w(z2, pbar2, w2, xi2)
+result2 = worstcase_l1_w(z2, p̄2, w2, ξ2)
+
 println("Test case 3 (worstcase_l1_w):")
 println("For different weights: ", w2)
 println("Updated Probability Distribution: ", result2[1])
 println("Objective Value: ", result2[2])
+println("")
+
+# Test case 4
+
+z3 = 2*rand(Dirichlet(5, 5))
+p̄3 = rand(Dirichlet(5, 5))
+w3 = 10*rand(Dirichlet(5, 5))
+
+result3 = worstcase_l1_w(z3, p̄3, w3, ξ2)
+println("Test case 4 (worstcase_l1_w):")
+println("For pbar: ", p̄3)
+println("For different weights: ", w3)
+println("Updated Probability Distribution: ", result3[1])
+println("Objective Value: ", result3[2])
 println("")
